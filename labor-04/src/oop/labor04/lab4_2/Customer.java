5f9c7c654f23e532b518687144c7125e7c2b8435
GerebKrisztina <gereb.krisztina@student.ms.sapientia.ro>
2023-03-14 10:07:43 +0200

    Labor4

--- labor-04/src/oop/labor04/lab4_2/Customer.java
+++ labor-04/src/oop/labor04/lab4_2/Customer.java
@@ -1,18 +1,19 @@
 package oop.labor04.lab4_2;
 
-import oop.labor03.lab3_1.BankAccount;
-
 import java.util.ArrayList;
 
-public class Customer {
-    private String firstName;
+public class Costumer {
+
+    private final String firstName;
     private String lastName;
+    private BankAccount account;
     private ArrayList<BankAccount> accounts = new ArrayList<>();
-    public Customer(String firstName, String lastName) {
+
+    public Costumer(String firstName, String lastName) {
         this.firstName = firstName;
         this.lastName = lastName;
     }
-11
+
     public String getFirstName() {
         return firstName;
     }
@@ -21,32 +22,24 @@ public class Customer {
         return lastName;
     }
 
+    public BankAccount getAccount() {
+        return account;
+    }
+
     public void setLastName(String lastName) {
         this.lastName = lastName;
     }
 
-    public BankAccount getAccount(String accountNumber) {
-        for(BankAccount na : accounts){
-            if(ba.getAccountNumber().equals(accountNumber)) {
-                return ba;
-            }
-        }
-        return null;
-    }
-    public void addAccount(BankAccount account) {
-        if(numAccounts< MAX_ACCOUNTS){
-            accounts[numAccounts] = account;
-            numAccounts++;
-        }
+    public void setAccount(BankAccount account) {
+        this.account = account;
     }
 
+    public void addAccount(BankAccount account) { accounts.add(account); }
 
     @Override
     public String toString() {
-        return firstName + " " + lastName + ": " + accounts;
-    }
-    public void closeAccount(){
-        this.accounts = null;
+        return account + " szamla tulajdonosa: " + this.firstName + " " + this.lastName;
     }
 
+    public void closeAccount() { this.account = null; }
 }
